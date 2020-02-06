ab4487c162b5ba5d2ed8cd39887b7a13ef61c14c
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2020-02-06 04:11:13 -0500

    dialogs

---
+++ src/DialogAdd.svelte
@@ -0,0 +1,38 @@
+<script>
+
+  import {onMount} from 'svelte';
+
+  import New from "New.svelte";
+
+  export let title = 'Add';
+
+  export let destroy = true;
+  export let content = `
+    <input type="text" placeholder="a"></input>
+    <br/>
+    ->
+    <br/>
+    <input type="text" placeholder="b"></input>
+  `;
+
+  export let confirm = "OK";
+  export let deny = "";
+
+  onMount(()=>{
+
+
+
+  })
+
+</script>
+
+<New
+  title={title}
+  destroy={destroy}
+  content={content}
+  confirm={confirm}
+  deny={deny}
+  open={true}
+  >
+
+</New>
---
+++ src/DialogWelcome.svelte
@@ -0,0 +1,27 @@
+<script>
+
+  import {onMount} from 'svelte';
+
+  import New from "New.svelte";
+  import "DialogWelcome.svelte.scss";
+
+  export let title = 'Welcome!';
+
+  export let destroy = true;
+  export let content = 'Thank you for being one of the first to test out my GraphViz based applicaiton for editing and manipulating GraphViz digraphs. If you like to see more of my work see: <a href="https://www.ryanspice.com/">ryanspice.com</a>';
+
+  export let confirm = "OK";
+  export let deny = "";
+
+
+</script>
+
+<New
+  title={title}
+  destroy={destroy}
+  content={content}
+  confirm={confirm}
+  deny={deny}
+  >
+
+</New>
