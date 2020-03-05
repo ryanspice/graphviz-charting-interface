040f511675e194579f84186a1df0dc6fa3fd92e3
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2020-03-05 02:44:32 -0500

    cleanup

--- src/components/static/Dialog.svelte
+++ src/components/static/Dialog.svelte
@@ -1,7 +1,6 @@
 <script>
 
   import {
-    onMount,
     afterUpdate
   } from "svelte";
 
@@ -17,32 +16,18 @@
     Label
   } from '@smui/button';
 
-  import List, {
-    Item,
-    Graphic,
-    Text
-  } from '@smui/list';
-
-/*
-  import Radio from '@smui/radio';
-  import Slider from '@smui/slider';
-  import FormField from '@smui/form-field';
-*/
+  export let id;
   export let title = 'undefined';
-  export let destroy = false;
   export let open = true;
   export let confirm = "true";
   export let onConfirm = () => {
 
   };
   export let deny = "false";
-  export let content = ``;
-  export let id;
-  export let components = [];
 
   let self;
 
-  function onCloseHandler(){
+  const onCloseHandler  = function(){
     onConfirm();
     self.$destroy();
   }
@@ -60,17 +45,21 @@
 </script>
 
 <Dialog
-    bind:this={self}
-    on:MDCDialog:closed={onCloseHandler}
-    id={"dialog-welcome"}
-    class="puff-in-center" aria-labelledby="slider-title" aria-describedby="slider-content">
+  id={id}
+  bind:this={self}
+  on:MDCDialog:closed={onCloseHandler}
+  class="puff-in-center" aria-labelledby="slider-title" aria-describedby="slider-content">
 
   {#if (title)}
+
       <Title id="slider-title">{title}</Title>
+
   {/if}
 
   <Content id={id}>
+
     <slot />
+
   </Content>
 
   {#if (deny||confirm)}
